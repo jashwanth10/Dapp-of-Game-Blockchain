8bb05f655d61f03a082fc71525f1ddffc9f01701
jashwanth10 <jashwanth.gajjala@gmail.com>
2019-11-10 06:45:30 +0530

    first

--- src/js/app.js
+++ src/js/app.js
@@ -4,55 +4,106 @@ App = {
 
   init: async function() {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
+    // console.log("HIHI");
     return await App.initWeb3();
   },
 
   initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.enable();
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+        console.log("error")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    acc = web3.eth.accounts[0];
+    
+    $.getJSON('Basic.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var AdoptionArtifact = data;
+      App.contracts.Basic = TruffleContract(AdoptionArtifact);
+      console.log(App.contracts.Basic);
+    
+      // Set the provider for our contract
+      App.contracts.Basic.setProvider(App.web3Provider);
+    
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '.one', App.handleAdopt);
+    $(document).on('click', '.multi', App.handleAdopt1);
+
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    console.log("HIHIIII")
   },
 
   handleAdopt: function(event) {
     event.preventDefault();
+    // var basicInstance;
+    var adoptionInstance;
 
-    var petId = parseInt($(event.target).data('id'));
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+    
+      var account = accounts[0];
+    
+      App.contracts.Basic.deployed().then(function(instance) {
+        adoptionInstance = instance;
+        console.log(instance);
+      
+        // Execute adopt as a transaction by sending account
+        return adoptionInstance.set(11);
+      }).then(function(result) {
+        console.log(result);
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+    // var petId = parseInt($(event.target).data('id'));
+    // console.log(petId);
+
+    /*
+     * Replace me...
+     */
+  },
+  handleAdopt1: function(event) {
+    event.preventDefault();
+    var basicInstance;
+    App.contracts.Basic.deployed().then(function(instance){
+      basicInstance = instance;
+      return basicInstance.get();
+    }).then(function(result){
+      console.log(result);
+    });
+    // var petId = parseInt($(event.target).data('id'));
+    // console.log(petId);
 
     /*
      * Replace me...
@@ -62,7 +113,11 @@ App = {
 };
 
 $(function() {
-  $(window).load(function() {
+  // $(window).load(function() {
+  //     App.init();
+  // });
+  $("#sign").click(function(event){
     App.init();
   });
+  
 });
